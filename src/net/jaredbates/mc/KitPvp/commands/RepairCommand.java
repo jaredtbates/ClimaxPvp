5263140c92ee669978d20c9ade9c23dc854a3f0e
computerwizjared <computerwizjared@hotmail.com>
2014-08-11 09:54:32 -0500

    Added /repair :D

--- src/net/jaredbates/mc/KitPvp/commands/RepairCommand.java
+++ src/net/jaredbates/mc/KitPvp/commands/RepairCommand.java
@@ -2,22 +2,36 @@ package net.jaredbates.mc.KitPvp.commands;
 
 import net.jaredbates.mc.KitPvp.Main;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class RepairCommand implements Listener {
 	Main plugin;
-	
+
 	public RepairCommand(Main plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
 		String[] args = event.getMessage().substring(1).split(" ");
-		if (args[0].equals("/1v1")) {
-			event.getPlayer().sendMessage("Test");
+		Player player = event.getPlayer();
+		if (args[0].equals("repair")) {
+			event.setCancelled(true);
+			if (player.hasPermission("ClimaxKits.Repair")) {
+				for (ItemStack item : player.getInventory().getContents()) {
+					item.setDurability((short) 0);
+				}
+				for (ItemStack item : player.getInventory().getArmorContents()) {
+					item.setDurability((short) 0);
+				}
+				player.sendMessage("§aYour inventory has been repaired!");
+			} else {
+				player.sendMessage("§cYou do not have permission for that command!");
+			}
 		}
 	}
 }
