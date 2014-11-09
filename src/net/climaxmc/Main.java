fe8b7de74ae2e725cacee69230b252e48cfbe00a
Jared Bates <computerwizjared@hotmail.com>
2014-11-09 16:02:57 -0600

    Passwords = Waste of Time -.-

--- src/net/climaxmc/Main.java
+++ src/net/climaxmc/Main.java
@@ -1,14 +1,9 @@
 package net.climaxmc;
 
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.UUID;
-
 import net.climaxmc.KitPvp.KitPvp;
-import net.climaxmc.KitPvp.Commands.*;
-import net.climaxmc.Listeners.*;
+import net.climaxmc.KitPvp.Commands.RepairCommand;
+import net.climaxmc.KitPvp.Commands.SpawnCommand;
 import net.climaxmc.OneVsOne.OneVsOne;
-import net.climaxmc.Utils.Title;
 import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -17,25 +12,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	public String climax = "§0§l[§cClimax§0§l] §r";
 	public Economy economy = null;
-	public Connection connection;
-	public Title passwordTitle = new Title("§c§lENTER YOUR PASSWORD", "", 0, 12000, 0);
-	public ArrayList<UUID> inPassword = new ArrayList<UUID>();
 	
 	public void onEnable() {
 		saveDefaultConfig();
-		//connection = Database.openConnection(this);
 		setupEconomy();
 		new KitPvp(this);
 		new OneVsOne(this);
-		//getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
-		//getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
-		//getServer().getPluginManager().registerEvents(new AsyncPlayerChatListener(this), this);
 		getCommand("repair").setExecutor(new RepairCommand(this));
 		getCommand("spawn").setExecutor(new SpawnCommand(this));
 	}
 	
 	public void onDisable() {
-		//Database.closeConnection(this, connection);
+		
 	}
 
 	private boolean setupEconomy() {
