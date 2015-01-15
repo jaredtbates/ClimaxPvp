6c74b99886a4fe7589cba3db6f8c4455c92e3ba1
computerwizjared <computerwizjared@hotmail.com>
2015-01-15 14:49:38 -0600

    Added some witch stuff

--- src/main/java/net/climaxmc/KitPvp/Kits/WitchKit.java
+++ src/main/java/net/climaxmc/KitPvp/Kits/WitchKit.java
@@ -2,15 +2,25 @@ package net.climaxmc.KitPvp.Kits;
 
 import net.climaxmc.KitPvp.Kit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionType;
 
 public class WitchKit extends Kit {
     public WitchKit() {
-        super("Witch", new ItemStack(Material.POTION), "", KitType.AMATEUR);
+        super("Witch", new ItemStack(Material.SOUL_SAND), "Potions FTW!", KitType.AMATEUR);
     }
 
     public void wear(Player player) {
-
+        player.getInventory().setHelmet(new ItemStack(Material.IRON_HELMET));
+        player.getInventory().setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
+        player.getInventory().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
+        player.getInventory().setBoots(new ItemStack(Material.CHAINMAIL_BOOTS));
+        ItemStack sword = new ItemStack(Material.STONE_SWORD);
+        sword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
+        player.getInventory().addItem(sword);
+        addSoup(player.getInventory(), 3, 35);
     }
 }
