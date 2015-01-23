d49a9ec38a7afee687ee5e35005327a5978d5e9c
computerwizjared <computerwizjared@hotmail.com>
2015-01-22 20:11:11 -0600

    Stuff

--- src/main/java/net/climaxmc/Donations/Listeners/PlayerMoveListener.java
+++ src/main/java/net/climaxmc/Donations/Listeners/PlayerMoveListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
 
 public class PlayerMoveListener implements Listener {
     private ClimaxPvp plugin;
@@ -20,15 +21,21 @@ public class PlayerMoveListener implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        if (instance.getParticlesEnabled().containsKey(player.getUniqueId())) {
-            for (ParticleEffect.ParticleType type : ParticleEffect.ParticleType.values()) {
-                if (instance.getParticlesEnabled().get(player.getUniqueId()).getType().equals(type)) {
-                    Location location = player.getLocation();
-                    location.setY(location.getY() + 0.5);
-                    new ParticleEffect(instance.getParticlesEnabled().get(player.getUniqueId())).sendToLocation(location);
+        final Player player = event.getPlayer();
+        if (!(event.getTo().getBlockX() == player.getLocation().getBlockX())) {
+            plugin.getServer().getScheduler().runTaskLater(plugin, new Runnable() {
+                public void run() {
+                    if (instance.getParticlesEnabled().containsKey(player.getUniqueId())) {
+                        for (ParticleEffect.ParticleType type : ParticleEffect.ParticleType.values()) {
+                            if (instance.getParticlesEnabled().get(player.getUniqueId()).getType().equals(type)) {
+                                Location location = player.getLocation();
+                                location.setY(location.getY() + 0.5);
+                                new ParticleEffect(instance.getParticlesEnabled().get(player.getUniqueId())).sendToLocation(location);
+                            }
+                        }
+                    }
                 }
-            }
+            }, 20);
         }
     }
 }
