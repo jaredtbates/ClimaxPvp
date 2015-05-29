48ef3c9d06e7aeb4637ee68b8eaf4e89bdfde352
computerwizjared <computerwizjared@hotmail.com>
2015-05-29 09:04:53 -0500

    Add methods and javadoc to PlayerData

--- src/main/java/net/climaxmc/API/PlayerData.java
+++ src/main/java/net/climaxmc/API/PlayerData.java
@@ -16,15 +16,75 @@ public class PlayerData {
     @Getter
     private int deaths;
 
-    public void setBalance(int balance) {
-        mySQL.updateData("balance", Integer.toString(this.balance = balance), player);
+    /**
+     * Sets the player's balance
+     * @param amount Amount to set
+     */
+    public void setBalance(int amount) {
+        mySQL.updateData("balance", Integer.toString(balance = amount), player);
     }
 
-    public void setKills(int kills) {
-        mySQL.updateData("kills", Integer.toString(this.kills = kills), player);
+    /**
+     * Deposits to player's balance
+     * @param amount Amount to deposit
+     */
+    public void depositBalance(int amount) {
+        setBalance(balance + amount);
     }
 
-    public void setDeaths(int deaths) {
-        mySQL.updateData("deaths", Integer.toString(this.deaths = deaths), player);
+    /**
+     * Withdraws from player's balance
+     * @param amount Amount to withdraw
+     */
+    public void withdrawBalance(int amount) {
+        setBalance(balance - amount);
+    }
+
+    /**
+     * Sets the player's kills
+     * @param amount Amount to set
+     */
+    public void setKills(int amount) {
+        mySQL.updateData("kills", Integer.toString(kills = amount), player);
+    }
+
+    /**
+     * Adds to player's kills
+     * @param amount Amount to add
+     */
+    public void addKills(int amount) {
+        setKills(kills + amount);
+    }
+
+    /**
+     * Removes from player's kills
+     * @param amount Amount to remove
+     */
+    public void removeKills(int amount) {
+        setKills(kills - amount);
+    }
+
+    /**
+     * Sets the player's deaths
+     * @param amount Amount to set
+     */
+    public void setDeaths(int amount) {
+        mySQL.updateData("deaths", Integer.toString(deaths = amount), player);
+    }
+
+    /**
+     * Adds to player's deaths
+     * @param amount Amount to add
+     */
+    public void addDeaths(int amount) {
+        setDeaths(deaths + amount);
+    }
+
+    /**
+     * Removes from player's deaths
+     * @param amount Amount to remove
+     */
+    public void removeDeaths(int amount) {
+        setDeaths(deaths - amount);
     }
 }
