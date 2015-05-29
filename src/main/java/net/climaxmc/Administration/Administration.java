3b9a218e3b5765aac934bfba6b484a5013301b68
computerwizjared <computerwizjared@hotmail.com>
2015-05-29 13:50:08 -0500

    Register rank command

--- src/main/java/net/climaxmc/Administration/Administration.java
+++ src/main/java/net/climaxmc/Administration/Administration.java
@@ -1,12 +1,17 @@
 package net.climaxmc.Administration;
 
+import net.climaxmc.Administration.Commands.RankCommand;
 import net.climaxmc.Administration.Listeners.AsyncPlayerChatListener;
 import net.climaxmc.Administration.Listeners.PlayerCommandPreprocessListener;
 import net.climaxmc.ClimaxPvp;
 
 public class Administration {
     public Administration(ClimaxPvp plugin) {
+        // Register listeners
         plugin.getServer().getPluginManager().registerEvents(new PlayerCommandPreprocessListener(plugin), plugin);
         plugin.getServer().getPluginManager().registerEvents(new AsyncPlayerChatListener(plugin), plugin);
+
+        // Register commands
+        plugin.getCommand("rank").setExecutor(new RankCommand(plugin));
     }
 }
