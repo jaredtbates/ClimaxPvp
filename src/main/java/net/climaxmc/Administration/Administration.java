7b0f5c85ef069c3f722bd71340b89d2619cf1f9f
computerwizjared <computerwizjared@hotmail.com>
2015-05-29 13:49:08 -0500

    Register chat listener

--- src/main/java/net/climaxmc/Administration/Administration.java
+++ src/main/java/net/climaxmc/Administration/Administration.java
@@ -1,10 +1,12 @@
 package net.climaxmc.Administration;
 
+import net.climaxmc.Administration.Listeners.AsyncPlayerChatListener;
 import net.climaxmc.Administration.Listeners.PlayerCommandPreprocessListener;
 import net.climaxmc.ClimaxPvp;
 
 public class Administration {
     public Administration(ClimaxPvp plugin) {
         plugin.getServer().getPluginManager().registerEvents(new PlayerCommandPreprocessListener(plugin), plugin);
+        plugin.getServer().getPluginManager().registerEvents(new AsyncPlayerChatListener(plugin), plugin);
     }
 }
