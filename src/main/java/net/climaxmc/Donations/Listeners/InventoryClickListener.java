bfe9986d70e28a00e46b8ad457778d51846d28ec
computerwizjared <computerwizjared@hotmail.com>
2015-06-04 16:49:21 -0500

    More trail stuff

--- src/main/java/net/climaxmc/Donations/Listeners/InventoryClickListener.java
+++ src/main/java/net/climaxmc/Donations/Listeners/InventoryClickListener.java
@@ -1,8 +1,10 @@
 package net.climaxmc.Donations.Listeners;
 
+import net.climaxmc.API.PlayerData;
 import net.climaxmc.ClimaxPvp;
 import net.climaxmc.Donations.Donations;
 import net.climaxmc.Donations.Enums.Trail;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,21 +27,23 @@ public class InventoryClickListener implements Listener {
         if (inventory.getName().equals("§a§lTrail Settings")) {
             event.setCancelled(true);
             player.closeInventory();
-            Trail trail = null;
 
-            for (Trail possibleTrail : Trail.values()) {
-                if (event.getCurrentItem().getType().equals(possibleTrail.getMaterial())) {
-                    trail = possibleTrail;
-                }
-            }
+            for (Trail trail : Trail.values()) {
+                if (event.getCurrentItem().getType().equals(trail.getMaterial())) {
+                    PlayerData playerData = plugin.getPlayerData(player);
 
-            if (trail != null) {
-                if (instance.getTrailsEnabled().containsKey(player.getUniqueId()) && instance.getTrailsEnabled().get(player.getUniqueId()).equals(trail)) {
-                    instance.getTrailsEnabled().remove(player.getUniqueId());
-                    player.sendMessage("§aYou have removed your trail!");
-                } else {
-                    instance.getTrailsEnabled().put(player.getUniqueId(), trail);
-                    player.sendMessage("§aYou have applied the " + trail.getName() + " trail!");
+                    if (!playerData.hasPerk(trail)) {
+                        player.sendMessage(ChatColor.RED + "Please donate at https://donate.climaxmc.net for access to this trail!");
+                        return;
+                    }
+
+                    if (instance.getTrailsEnabled().containsKey(player.getUniqueId()) && instance.getTrailsEnabled().get(player.getUniqueId()).equals(trail)) {
+                        instance.getTrailsEnabled().remove(player.getUniqueId());
+                        player.sendMessage(ChatColor.GREEN + "You have removed your trail!");
+                    } else {
+                        instance.getTrailsEnabled().put(player.getUniqueId(), trail);
+                        player.sendMessage(ChatColor.GREEN + "You have applied the " + trail.getName() + " trail!");
+                    }
                 }
             }
         }
