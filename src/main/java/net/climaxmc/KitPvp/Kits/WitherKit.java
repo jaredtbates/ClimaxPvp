39db0ec14be81207c310057f4093be49846e5d56
jonhan2000 <jtccornett2000@gmail.com>
2015-06-05 17:00:47 -0400

    Magical Withery Kit

--- src/main/java/net/climaxmc/KitPvp/Kits/WitherKit.java
+++ src/main/java/net/climaxmc/KitPvp/Kits/WitherKit.java
@@ -1,14 +1,78 @@
 package net.climaxmc.KitPvp.Kits;
 
-import net.climaxmc.KitPvp.Kit;
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.WitherSkull;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+import net.climaxmc.KitPvp.Kit;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class WitherKit extends Kit {
+
+    ArrayList<UUID> wither = new ArrayList<UUID>();
+
     public WitherKit() {
-        super("Wither", null, null, null);
+        super("Wither", new ItemStack(Material.SKULL_ITEM), "Shoot your WitherBow to Launch your Wither Head!", ChatColor.RED);
     }
 
     public void wear(Player player) {
+        player.getInventory().addItem(new ItemStack(Material.IRON_SWORD));
+        player.getInventory().addItem(new ItemStack(Material.BOW));
+        player.getInventory().setHelmet(new ItemStack(Material.GOLD_HELMET));
+        player.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+        player.getInventory().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
+        ItemStack boots = new ItemStack(Material.GOLD_BOOTS);
+        boots.addUnsafeEnchantment(Enchantment.PROTECTION_FALL, 5);
+        player.getInventory().setBoots(boots);
+        addSoup(player.getInventory(), 1, 35);
+        wither.add(player.getUniqueId());
+    }
 
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        final Player player = event.getPlayer();
+        if (wither.contains(player.getUniqueId())) {
+            if (player.getInventory().getItemInHand().getType() == Material.IRON_SWORD) {
+                if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                    player.launchProjectile(WitherSkull.class).setVelocity(event.getProjectile().getVelocity());
+                }
+            }
+        }
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (wither.contains(player.getUniqueId())) {
+            wither.remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if (wither.contains(player.getUniqueId())) {
+            wither.remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        if (wither.contains(player.getUniqueId())) {
+            wither.remove(player.getUniqueId());
+        }
     }
 }
