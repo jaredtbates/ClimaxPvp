8ed971dd638ee7e37d0ae0c2e8afd1bd9064fe3f
computerwizjared <computerwizjared@hotmail.com>
2015-06-05 20:18:32 -0500

    Fix CombatLog

--- src/main/java/net/climaxmc/Administration/Listeners/CombatLogListeners.java
+++ src/main/java/net/climaxmc/Administration/Listeners/CombatLogListeners.java
@@ -18,26 +18,30 @@ public class CombatLogListeners implements Listener {
         this.plugin = plugin;
     }
 
-    private List<UUID> tagged = new ArrayList<>();
+    private Map<UUID, Integer> tagged = new HashMap<>();
 
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player && event.getEntity() instanceof Player){
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
             Player damager = (Player) event.getDamager();
             Player damaged = (Player) event.getEntity();
-            if (!(tagged.contains(damager.getUniqueId()) && tagged.contains(damaged.getUniqueId()))) {
-                tagged.add(damager.getUniqueId());
-                tagged.add(damaged.getUniqueId());
+            if (!(tagged.containsKey(damager.getUniqueId()) && tagged.containsKey(damaged.getUniqueId()))) {
+                tagged.put(damager.getUniqueId(), 10);
+                tagged.put(damaged.getUniqueId(), 10);
                 damager.sendMessage(ChatColor.GRAY + "You are now in combat with " + ChatColor.GOLD + damaged.getName() + ChatColor.GRAY + ".");
                 damaged.sendMessage(ChatColor.GRAY + "You are now in combat with " + ChatColor.GOLD + damager.getName() + ChatColor.GRAY + ".");
-                new BukkitRunnable(){
-                    int tagTime = 10;
-                    public void run(){
-                        if(tagTime-- == 0){
-                            tagged.remove(damaged.getUniqueId());
+                new BukkitRunnable() {
+                    public void run() {
+                        tagged.put(damager.getUniqueId(), tagged.get(damager.getUniqueId()) - 1);
+                        if (tagged.get(damager.getUniqueId()) == 0) {
                             tagged.remove(damager.getUniqueId());
-                            damaged.sendMessage(ChatColor.GRAY + "You are no longer in combat.");
                             damager.sendMessage(ChatColor.GRAY + "You are no longer in combat.");
+                        }
+
+                        tagged.put(damaged.getUniqueId(), tagged.get(damaged.getUniqueId()) - 1);
+                        if (tagged.get(damager.getUniqueId()) == 0) {
+                            tagged.remove(damaged.getUniqueId());
+                            damaged.sendMessage(ChatColor.GRAY + "You are no longer in combat.");
                         }
                     }
                 }.runTaskTimer(plugin, 20, 20);
@@ -46,10 +50,10 @@ public class CombatLogListeners implements Listener {
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event){
+    public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
 
-        if (tagged.contains(player.getUniqueId())){
+        if (tagged.containsKey(player.getUniqueId())) {
             player.damage(Integer.MIN_VALUE);
             plugin.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + player.getName() + ChatColor.RED + " has logged out while in combat!");
         }
