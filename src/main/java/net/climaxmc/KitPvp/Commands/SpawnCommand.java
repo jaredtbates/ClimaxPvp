47f991fef827fd484ea87504ad377a59bd0962db
computerwizjared <computerwizjared@hotmail.com>
2015-06-06 09:21:26 -0500

    Change name of final jar

--- src/main/java/net/climaxmc/KitPvp/Commands/SpawnCommand.java
+++ src/main/java/net/climaxmc/KitPvp/Commands/SpawnCommand.java
@@ -3,7 +3,6 @@ package net.climaxmc.KitPvp.Commands;
 import net.climaxmc.ClimaxPvp;
 import net.climaxmc.KitPvp.KitPvp;
 import org.bukkit.ChatColor;
-import org.bukkit.block.Block;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
@@ -20,22 +19,13 @@ public class SpawnCommand implements CommandExecutor {
             return true;
         }
 
-        final Player player = (Player) sender;
-        final Block block = player.getLocation().getBlock();
+        Player player = (Player) sender;
 
-        player.sendMessage(ChatColor.GREEN + "Please wait " + ChatColor.RED + "3" + ChatColor.GREEN + " seconds to be teleported to spawn.");
-
-        plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
-            if (player.getLocation().getBlock().equals(block)) {
-                if (KitPvp.inKit.contains(player.getUniqueId())) {
-                    KitPvp.inKit.remove(player.getUniqueId());
-                }
-                plugin.respawn(player);
-                player.sendMessage(ChatColor.GREEN + "You have been teleported to spawn!");
-            } else {
-                player.sendMessage(ChatColor.RED + "Teleportation canceled.");
-            }
-        }, 60);
+        if (KitPvp.inKit.contains(player.getUniqueId())) {
+            KitPvp.inKit.remove(player.getUniqueId());
+        }
+        plugin.respawn(player);
+        player.sendMessage(ChatColor.GREEN + "You have been teleported to spawn!");
 
         return true;
     }
