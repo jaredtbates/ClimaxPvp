262fafed80f22a8a9b96c5091213df160319b9d1
jonhan2000 <jtccornett2000@gmail.com>
2015-06-06 21:36:42 -0400

    Added the most important part of SanicKit to sanic...

--- src/main/java/net/climaxmc/KitPvp/Kits/SanicKit.java
+++ src/main/java/net/climaxmc/KitPvp/Kits/SanicKit.java
@@ -1,11 +1,14 @@
 package net.climaxmc.KitPvp.Kits;
 
 import net.climaxmc.KitPvp.Kit;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class SanicKit extends Kit {
 
@@ -14,12 +17,16 @@ public class SanicKit extends Kit {
     }
 
     public void wear(Player player) {
+    	for (PotionEffect effect : player.getActivePotionEffects()) {
+            player.removePotionEffect(effect.getType());
+        }
     	ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
     	sword.addEnchantment(Enchantment.DAMAGE_ALL, 3);
         player.getInventory().addItem(sword);
         ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS);
         boots.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
         player.getInventory().setBoots(boots);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 3));
         addSoup(player.getInventory(), 1, 35);
     }
 }
