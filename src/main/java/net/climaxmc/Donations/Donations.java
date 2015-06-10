890488455aa0f5e75a154834d4cd0c271413aa69
computerwizjared <computerwizjared@hotmail.com>
2015-06-10 18:17:36 -0500

    Donation stuff

--- src/main/java/net/climaxmc/Donations/Donations.java
+++ src/main/java/net/climaxmc/Donations/Donations.java
@@ -6,7 +6,6 @@ import net.climaxmc.Donations.Commands.*;
 import net.climaxmc.Donations.Enums.Trail;
 import net.climaxmc.Donations.Listeners.InventoryClickListener;
 import net.climaxmc.Donations.Listeners.PlayerMoveListener;
-import net.climaxmc.Donations.Web.CommandChecker;
 
 import java.util.*;
 
@@ -15,13 +14,6 @@ public class Donations {
     private Map<UUID, Trail> trailsEnabled = new HashMap<>();
 
     public Donations(ClimaxPvp plugin) {
-        // Initialize command checker for Minecraft Market
-        if (plugin.getConfig().getString("Donations.APIKey").matches("[0-9a-f]+") && plugin.getConfig().getString("Donations.APIKey").length() == 32) {
-            new CommandChecker(plugin).runTaskTimerAsynchronously(plugin, 600L, 1200L);
-        } else {
-            plugin.getLogger().severe("Donations API key invalid! Disabling Minecraft Market functionality!");
-        }
-
         // Register Commands
         plugin.getCommand("perk").setExecutor(new PerkCommand(plugin));
         plugin.getCommand("trails").setExecutor(new TrailsCommand(plugin));
