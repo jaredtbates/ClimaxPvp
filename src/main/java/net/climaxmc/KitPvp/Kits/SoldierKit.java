4db16755ae0ef328d82e049b3af2fc14a9b149b7
jonhan2000 <jtccornett2000@gmail.com>
2015-06-18 20:40:15 -0400

    Tried to add durability thing to soldier, we'll see.

--- src/main/java/net/climaxmc/KitPvp/Kits/SoldierKit.java
+++ src/main/java/net/climaxmc/KitPvp/Kits/SoldierKit.java
@@ -37,6 +37,7 @@ public class SoldierKit extends Kit {
             if (player.getInventory().getItemInHand().getType() == Material.IRON_SWORD) {
                 if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
                     player.setVelocity(new Vector(0, 0.5, 0));
+                    player.getInventory().getItemInHand().setDurability((short) (player.getInventory().getItemInHand().getDurability() - 20));
                 }
             }
         }
