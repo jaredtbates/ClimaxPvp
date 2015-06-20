1b8bba9903c80a6c188b9f143deb5f4c126a0e20
jonhan2000 <jtccornett2000@gmail.com>
2015-06-20 17:03:42 -0400

    Added rabbit kit to KitManager

---
+++ src/main/java/net/climaxmc/KitPvp/Kits/RabbitKit.java
@@ -0,0 +1,51 @@
+package net.climaxmc.KitPvp.Kits;
+
+import net.climaxmc.KitPvp.Kit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class RabbitKit extends Kit {
+    public RabbitKit() {
+        super("Rabbit", new ItemStack(Material.CARROT), "Gain Jump-Boost 2 while Fighting!", ChatColor.BLUE);
+    }
+
+    protected void wear(Player player) {
+    	for (PotionEffect effect : player.getActivePotionEffects()) {
+            player.removePotionEffect(effect.getType());
+        }
+    	player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 1));
+        ItemStack sword = new ItemStack(Material.IRON_SWORD);
+        sword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
+        player.getInventory().addItem(sword);
+        player.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+        player.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+        ItemStack boots = new ItemStack(Material.IRON_BOOTS);
+        boots.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2);
+        player.getInventory().setBoots(boots);
+        addSoup(player.getInventory(), 1, 35);
+    }
+
+    protected void wearNoSoup(Player player) {
+    	for (PotionEffect effect : player.getActivePotionEffects()) {
+            player.removePotionEffect(effect.getType());
+        }
+    	player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 1));
+    	player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 0));
+        ItemStack sword = new ItemStack(Material.IRON_SWORD);
+        sword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
+        player.getInventory().addItem(sword);
+        player.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+        player.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+        ItemStack boots = new ItemStack(Material.IRON_BOOTS);
+        boots.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2);
+        player.getInventory().setBoots(boots);
+        ItemStack fishingRod = new ItemStack(Material.FISHING_ROD);
+        fishingRod.addEnchantment(Enchantment.DURABILITY, 3);
+        player.getInventory().addItem(fishingRod);
+    }
+}
