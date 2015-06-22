c8febf9304ced964f4b536d40a76409b967d42ad
computerwizjared <computerwizjared@hotmail.com>
2015-06-22 07:08:37 -0500

    Stuff

---
+++ src/main/java/net/climaxmc/KitPvp/Commands/HelpCommand.java
@@ -0,0 +1,44 @@
+package net.climaxmc.KitPvp.Commands;
+
+import net.climaxmc.ClimaxPvp;
+import net.climaxmc.common.database.PlayerData;
+import org.bukkit.ChatColor;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class HelpCommand implements CommandExecutor {
+    private ClimaxPvp plugin;
+
+    public HelpCommand(ClimaxPvp plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+
+        Player player = (Player) sender;
+        PlayerData playerData = plugin.getPlayerData(player);
+        if (playerData.getBalance() >= 2) {
+            playerData.withdrawBalance(2);
+            for (ItemStack item : player.getInventory().getContents()) {
+                if (item != null) {
+                    item.setDurability((short) -100);
+                }
+            }
+            for (ItemStack item : player.getInventory().getArmorContents()) {
+                if (item != null) {
+                    item.setDurability((short) -100);
+                }
+            }
+            player.sendMessage(ChatColor.GREEN + "You repaired your inventory for $2!");
+        } else {
+            player.sendMessage(ChatColor.RED + "You do not have enough money to repair your inventory!");
+        }
+
+        return true;
+    }
+}
