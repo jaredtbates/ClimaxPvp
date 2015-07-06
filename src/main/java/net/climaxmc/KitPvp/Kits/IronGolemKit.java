3250a69b2a2f418d2bae8e892bb75e0a99c22c73
jonhan2000 <jtccornett2000@gmail.com>
2015-07-06 11:35:20 -0400

    Re-enabled iron golem

--- src/main/java/net/climaxmc/KitPvp/Kits/IronGolemKit.java
+++ src/main/java/net/climaxmc/KitPvp/Kits/IronGolemKit.java
@@ -5,11 +5,14 @@ import net.climaxmc.KitPvp.KitManager;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 public class IronGolemKit extends Kit {
@@ -19,7 +22,7 @@ public class IronGolemKit extends Kit {
 
     protected void wear(Player player) {
     	player.sendMessage(ChatColor.GOLD + "This kit is " + ChatColor.RED + ChatColor.BOLD + "Disabled!");
-        /*ItemStack sword = new ItemStack(Material.GOLD_SWORD);
+        ItemStack sword = new ItemStack(Material.GOLD_SWORD);
         sword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
         sword.addEnchantment(Enchantment.DURABILITY, 2);
         player.getInventory().addItem(sword);
@@ -28,12 +31,12 @@ public class IronGolemKit extends Kit {
         player.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
         ItemStack boots = new ItemStack(Material.CHAINMAIL_BOOTS);
         player.getInventory().setBoots(boots);
-        addSoup(player.getInventory(), 2, 35);*/
+        addSoup(player.getInventory(), 2, 35);
     }
 
     protected void wearNoSoup(Player player) {
     	player.sendMessage(ChatColor.GOLD + "This kit is " + ChatColor.RED + ChatColor.BOLD + "Disabled!");
-    	/*for (PotionEffect effect : player.getActivePotionEffects()) {
+    	for (PotionEffect effect : player.getActivePotionEffects()) {
             player.removePotionEffect(effect.getType());
         }
     	player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 1));
@@ -48,7 +51,7 @@ public class IronGolemKit extends Kit {
         player.getInventory().setBoots(boots);
         ItemStack fishingRod = new ItemStack(Material.FISHING_ROD);
         fishingRod.addEnchantment(Enchantment.DURABILITY, 3);
-        player.getInventory().addItem(fishingRod);*/
+        player.getInventory().addItem(fishingRod);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
