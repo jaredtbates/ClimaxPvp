801eb84098a0acc12b4aaf4d42e392daf6a971b7
jonhan2000 <jtccornett2000@gmail.com>
2015-07-08 23:27:13 -0400

    Woops

--- src/main/java/net/climaxmc/KitPvp/Kits/ArcherKit.java
+++ src/main/java/net/climaxmc/KitPvp/Kits/ArcherKit.java
@@ -60,8 +60,8 @@ public class ArcherKit extends Kit {
     	for (PotionEffect effect : player.getActivePotionEffects()) {
             player.removePotionEffect(effect.getType());
         }
-    	player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 0));
-    	player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
+    	player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 1));
+    	player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0));
         ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
         LeatherArmorMeta helmetMeta = (LeatherArmorMeta) helmet.getItemMeta();
         helmetMeta.setColor(Color.WHITE);
