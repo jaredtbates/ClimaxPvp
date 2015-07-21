8c5078d30357929ed0d8ff110de4d3500126ad0b
computerwizjared <computerwizjared@hotmail.com>
2015-07-21 10:36:15 -0500

    Added pling to messaging

--- src/main/java/net/climaxmc/KitPvp/Commands/Messaging/ReplyCommand.java
+++ src/main/java/net/climaxmc/KitPvp/Commands/Messaging/ReplyCommand.java
@@ -3,6 +3,7 @@ package net.climaxmc.KitPvp.Commands.Messaging;
 import net.climaxmc.ClimaxPvp;
 import net.md_5.bungee.api.ChatColor;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Sound;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
@@ -40,6 +41,9 @@ public class ReplyCommand implements CommandExecutor {
 
         player.sendMessage(ChatColor.DARK_AQUA + player.getName() + ChatColor.BOLD + " -> " + ChatColor.AQUA + "" + ChatColor.BOLD + target.getName() + ChatColor.AQUA + ": " + message.trim());
         target.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + player.getName() + ChatColor.DARK_AQUA + "" + ChatColor.BOLD + " -> " + ChatColor.DARK_AQUA + target.getName() + ChatColor.AQUA + ": " + message);
+
+        player.playSound(player.getLocation(), Sound.NOTE_PIANO, 1, 1);
+        target.playSound(target.getLocation(), Sound.NOTE_PIANO, 2, 2);
 
         return true;
     }
