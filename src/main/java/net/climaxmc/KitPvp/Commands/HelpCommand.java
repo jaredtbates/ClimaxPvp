15617e1868ea0ce23628fab5d23bc23e2f5457bb
computerwizjared <computerwizjared@hotmail.com>
2015-07-23 13:22:58 -0500

    Added staff help

--- src/main/java/net/climaxmc/KitPvp/Commands/HelpCommand.java
+++ src/main/java/net/climaxmc/KitPvp/Commands/HelpCommand.java
@@ -1,6 +1,9 @@
 package net.climaxmc.KitPvp.Commands;
 
 import net.climaxmc.ClimaxPvp;
+import net.climaxmc.common.Rank;
+import net.climaxmc.common.database.CachedPlayerData;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
@@ -18,8 +21,30 @@ public class HelpCommand implements CommandExecutor {
         }
 
         Player player = (Player) sender;
+        CachedPlayerData playerData = plugin.getPlayerData(player);
+
+        if (args.length == 1 && args[0].equals("staff") && playerData.hasRank(Rank.HELPER)) {
+            player.sendMessage(ChatColor.RED + "/punish" + ChatColor.GRAY + " - Punish a player with warn/kick/tempmute" + (playerData.hasRank(Rank.MODERATOR) ? "/mute/tempban/ban" : ""));
+            player.sendMessage(ChatColor.RED + "/inventory" + ChatColor.GRAY + " - Check if a player is autosouping");
+            player.sendMessage(ChatColor.RED + "/check" + ChatColor.GRAY + " - Check if a player is hacking");
+
+            if (playerData.hasRank(Rank.MODERATOR)) {
+                player.sendMessage(ChatColor.RED + "/vanish" + ChatColor.GRAY + " - Vanish from the game (removes you from the player list and fake logs you out)");
+            }
+
+            if (playerData.hasRank(Rank.ADMINISTRATOR)) {
+                player.sendMessage(ChatColor.RED + "/admin" + ChatColor.GRAY + " - Enable/disable all kits (used mainly for testing)");
+                player.sendMessage(ChatColor.RED + "/economy" + ChatColor.GRAY + " - Access economy settings");
+            }
+
+            return true;
+        }
 
         player.sendMessage(plugin.getHelp().toArray(new String[plugin.getHelp().size()]));
+
+        if (playerData.hasRank(Rank.HELPER)) {
+            player.sendMessage(ChatColor.RED + "Execute " + ChatColor.GREEN + "/help staff" + ChatColor.RED + " for information on staff commands.");
+        }
 
         return true;
     }
