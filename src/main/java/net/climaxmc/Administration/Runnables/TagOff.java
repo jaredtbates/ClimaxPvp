eb118d826cab8d5b070ca28d3e84c78413f1a9d4
computerwizjared <computerwizjared@hotmail.com>
2015-07-23 16:13:22 -0500

    Created new combat log

---
+++ src/main/java/net/climaxmc/Administration/Runnables/TagOff.java
@@ -0,0 +1,32 @@
+package net.climaxmc.Administration.Runnables;
+
+import net.climaxmc.Administration.Listeners.CombatLogListeners;
+import net.climaxmc.ClimaxPvp;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class TagOff implements Runnable {
+    Player player;
+    ClimaxPvp plugin;
+
+    public TagOff(ClimaxPvp plugin, Player p, long ticks) {
+        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, this, ticks * 20L);
+        this.player = p;
+    }
+
+    public void run() {
+        String msg = ChatColor.GRAY + "You are no longer in combat.";
+        int tagtime = 15;
+
+        if (CombatLogListeners.tagged.contains(player.getUniqueId())) {
+            if (System.currentTimeMillis() - CombatLogListeners.tagTime.get(player.getUniqueId()) > tagtime * 1000 - 20) {
+                player.sendMessage(msg);
+                CombatLogListeners.tagged.remove(player.getUniqueId());
+                CombatLogListeners.tagTime.remove(player.getUniqueId());
+            } else {
+                long time = System.currentTimeMillis() / 1000L - CombatLogListeners.tagTime.get(player.getUniqueId()) / 1000L;
+                new TagOff(plugin, player, time);
+            }
+        }
+    }
+}
