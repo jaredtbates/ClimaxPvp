c29b74587eda83b9893eaea4515d7dcbe4608710
GamerBah <sourpickle247@gmail.com>
2015-08-02 15:46:21 -0700

    Added a message displaying the amount of health your opponent had when you died.

--- src/main/java/net/climaxmc/KitPvp/Listeners/PlayerDeathListener.java
+++ src/main/java/net/climaxmc/KitPvp/Listeners/PlayerDeathListener.java
@@ -53,8 +53,18 @@ public class PlayerDeathListener implements Listener {
 
         if (plugin.getServer().getOnlinePlayers().size() >= 15) {
             event.setDeathMessage(null);
+            if(((int) killer.getHealth() & 1) == 0) {
+                player.sendMessage(ChatColor.RED + killer.getName() + ChatColor.YELLOW + " had " + ChatColor.RED + (killer.getHealth() - 10) + " hearts" + ChatColor.YELLOW + " left");
+            } else {
+                player.sendMessage(ChatColor.RED + killer.getName() + ChatColor.YELLOW + " had " + ChatColor.RED + (killer.getHealth() - 11) + " 1/2 hearts" + ChatColor.YELLOW + " left");
+            }
         } else {
             event.setDeathMessage(ChatColor.RED + player.getName() + ChatColor.GRAY + " was killed by " + ChatColor.GREEN + killer.getName());
+            if(((int) killer.getHealth() & 1) == 0) {
+                player.sendMessage(ChatColor.RED + killer.getName() + ChatColor.YELLOW + " had " + ChatColor.RED + (killer.getHealth() - 10) + " hearts" + ChatColor.YELLOW + " left");
+            } else {
+                player.sendMessage(ChatColor.RED + killer.getName() + ChatColor.YELLOW + " had " + ChatColor.RED + (killer.getHealth() - 11) + " 1/2 hearts" + ChatColor.YELLOW + " left");
+            }
         }
 
         if (killer.getUniqueId().equals(player.getUniqueId())) {
