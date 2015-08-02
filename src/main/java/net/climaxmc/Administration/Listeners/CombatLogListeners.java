deb997e78dd01acb230ba5a8f398c3be16f1395c
computerwizjared <computerwizjared@hotmail.com>
2015-08-02 14:51:41 -0500

    Added banning upon combatlog

--- src/main/java/net/climaxmc/Administration/Listeners/CombatLogListeners.java
+++ src/main/java/net/climaxmc/Administration/Listeners/CombatLogListeners.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 public class CombatLogListeners implements Listener {
     private ClimaxPvp plugin;
     private HashMap<UUID, Integer> tagged = new HashMap<>();
+    private HashMap<UUID, Long> logged = new HashMap<>();
 
     public CombatLogListeners(ClimaxPvp plugin) {
         this.plugin = plugin;
@@ -38,7 +39,7 @@ public class CombatLogListeners implements Listener {
                                 tagged.remove(damaged.getUniqueId());
                                 damaged.sendMessage(ChatColor.GRAY + "You are no longer in combat.");
                             }
-                        }.runTaskLater(plugin, 300).getTaskId());
+                        }.runTaskLater(plugin, 40).getTaskId());
             } else {
                 plugin.getServer().getScheduler().cancelTask(tagged.get(damaged.getUniqueId()));
                 tagged.put(damaged.getUniqueId(),
@@ -47,7 +48,7 @@ public class CombatLogListeners implements Listener {
                                 tagged.remove(damaged.getUniqueId());
                                 damaged.sendMessage(ChatColor.GRAY + "You are no longer in combat.");
                             }
-                        }.runTaskLater(plugin, 300).getTaskId());
+                        }.runTaskLater(plugin, 40).getTaskId());
             }
 
             if (!tagged.containsKey(damager.getUniqueId())) {
@@ -58,7 +59,7 @@ public class CombatLogListeners implements Listener {
                                 tagged.remove(damager.getUniqueId());
                                 damager.sendMessage(ChatColor.GRAY + "You are no longer in combat.");
                             }
-                        }.runTaskLater(plugin, 300).getTaskId());
+                        }.runTaskLater(plugin, 40).getTaskId());
             } else {
                 plugin.getServer().getScheduler().cancelTask(tagged.get(damager.getUniqueId()));
                 tagged.put(damager.getUniqueId(),
@@ -67,7 +68,7 @@ public class CombatLogListeners implements Listener {
                                 tagged.remove(damager.getUniqueId());
                                 damager.sendMessage(ChatColor.GRAY + "You are no longer in combat.");
                             }
-                        }.runTaskLater(plugin, 300).getTaskId());
+                        }.runTaskLater(plugin, 40).getTaskId());
             }
         }
     }
@@ -79,6 +80,7 @@ public class CombatLogListeners implements Listener {
         if (tagged.containsKey(player.getUniqueId())) {
             tagged.remove(player.getUniqueId());
             plugin.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + player.getName() + ChatColor.RED + " has logged out while in combat!");
+            logged.put(player.getUniqueId(), System.currentTimeMillis() + 120000);
         }
     }
 
@@ -102,6 +104,19 @@ public class CombatLogListeners implements Listener {
         if (tagged.containsKey(player.getUniqueId())) {
             plugin.getServer().getScheduler().cancelTask(tagged.get(player.getUniqueId()));
             tagged.remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(AsyncPlayerPreLoginEvent event) {
+        UUID uuid = event.getUniqueId();
+
+        if (logged.containsKey(uuid)) {
+            if (System.currentTimeMillis() < logged.get(uuid)) {
+                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED, ChatColor.RED + "\nYou are banned for combat logging.\nYour ban will expire 2 minutes from when you were banned.");
+            } else {
+                logged.remove(uuid);
+            }
         }
     }
 }
