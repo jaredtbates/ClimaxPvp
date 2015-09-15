4a0558a9239c1eff569acfe37e407eedb2965a57
Jared Bates <me@jaredbates.net>
2015-09-15 16:29:21 -0500

    Fix scoreboard

--- src/main/java/net/climaxmc/KitPvp/Listeners/ScoreboardListener.java
+++ src/main/java/net/climaxmc/KitPvp/Listeners/ScoreboardListener.java
@@ -94,10 +94,6 @@ public class ScoreboardListener implements Listener {
             objective.getScore("   ").setScore(3);
             objective.getScore(ChatColor.YELLOW + "" + ChatColor.BOLD + "Website").setScore(2);
             objective.getScore("climaxmc.net").setScore(1);
-            Objective healthObjective = board.registerNewObjective("showhealth", "health");
-            healthObjective.setDisplaySlot(DisplaySlot.BELOW_NAME);
-            healthObjective.setDisplayName("\u2764");
-            player.setHealth(player.getHealth());
         }
     }
 }
