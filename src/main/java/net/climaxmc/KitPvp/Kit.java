c6ca2542135e1704724a4acf5671b5b984c0f40d
Jared Bates <me@jaredbates.net>
2015-11-07 13:29:54 -0600

    Fix radius from spawn for kit applying

--- src/main/java/net/climaxmc/KitPvp/Kit.java
+++ src/main/java/net/climaxmc/KitPvp/Kit.java
@@ -113,7 +113,7 @@ public abstract class Kit implements Listener, CommandExecutor {
      */
     public void wearCheckLevel(Player player) {
         PlayerData playerData = ClimaxPvp.getInstance().getPlayerData(player);
-        if (!KitManager.isPlayerInKit(player) || (playerData.hasRank(Rank.MASTER) && player.getLocation().distance(player.getWorld().getSpawnLocation()) < 250)) {
+        if (!KitManager.isPlayerInKit(player) || (playerData.hasRank(Rank.MASTER) && player.getLocation().distance(player.getWorld().getSpawnLocation()) < 200)) {
             if ((playerData.getLevelColor().contains(String.valueOf(color.getChar()))
                     || playerData.getTemporaryPlayerData().containsKey("Admin Mode")
                     || (((playerData.hasRank(Rank.NINJA) && (color.equals(ChatColor.BLUE) || color.equals(ChatColor.GREEN)))
@@ -132,7 +132,7 @@ public abstract class Kit implements Listener, CommandExecutor {
                 player.getInventory().clear();
                 player.getInventory().setArmorContents(null);
 
-                if (player.getLocation().distance(player.getWorld().getSpawnLocation()) < 150) {
+                if (player.getLocation().distance(player.getWorld().getSpawnLocation()) < 200) {
                     wearNoSoup(player);
                     return;
                 }
