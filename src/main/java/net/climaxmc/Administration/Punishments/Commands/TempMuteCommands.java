14bf2326d983eb1e254209e24975d98d09e8ab2c
Jared Bates <me@jaredbates.net>
2016-01-23 11:38:25 -0600

    trick git

--- src/main/java/net/climaxmc/Administration/Punishments/Commands/TempMuteCommands.java
+++ src/main/java/net/climaxmc/Administration/Punishments/Commands/TempMuteCommands.java
@@ -12,10 +12,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class TempMuteCommand implements CommandExecutor {
+public class TempMuteCommands implements CommandExecutor {
     private ClimaxPvp plugin;
 
-    public TempMuteCommand(ClimaxPvp plugin) {
+    public TempMuteCommands(ClimaxPvp plugin) {
         this.plugin = plugin;
     }
 
