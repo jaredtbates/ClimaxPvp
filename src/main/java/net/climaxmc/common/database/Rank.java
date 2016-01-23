d643b4a180f33067a754794bac1cd05887914bbb
Jared Bates <me@jaredbates.net>
2016-01-23 13:27:23 -0600

    re-add manager rank

--- src/main/java/net/climaxmc/common/database/Rank.java
+++ src/main/java/net/climaxmc/common/database/Rank.java
@@ -7,21 +7,8 @@ import net.md_5.bungee.api.ChatColor;
 @Getter
 @AllArgsConstructor
 public enum Rank {
-    /*OWNER("OWNER", ChatColor.RED, Integer.MAX_VALUE),
-    DEVELOPER("DEV", ChatColor.GOLD, 120),
-    ADMINISTRATOR("ADMIN", ChatColor.RED, 100),
-    MODERATOR("MOD", ChatColor.DARK_PURPLE, 80),
-    HEAD_BUILDER("BUILDER", ChatColor.AQUA, 70),
-    BUILDER("BUILDER", ChatColor.YELLOW, 60),
-    JR_DEVELOPER("JrDEV", ChatColor.GOLD, 50),
-    HELPER("HELPER", ChatColor.GREEN, 40),
-    TWITCH(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "TWI" + ChatColor.WHITE + "" + ChatColor.BOLD + "TCH", null, 30),
-    YOUTUBE(ChatColor.RED + "" + ChatColor.BOLD + "YOU" + ChatColor.WHITE + "" + ChatColor.BOLD + "TUBE", null, 30),
-    TRUSTED("TRUSTED", ChatColor.DARK_AQUA, 20),
-    MASTER("MASTER", ChatColor.DARK_RED, 3),
-    TITAN("TITAN", ChatColor.GOLD, 2),
-    NINJA("NINJA", ChatColor.AQUA, 1),*/
     OWNER("Owner", ChatColor.RED, Integer.MAX_VALUE),
+    MANAGER("Manager", ChatColor.RED, 150),
     DEVELOPER("Developer", ChatColor.GOLD, 120),
     ADMINISTRATOR("Admin", ChatColor.RED, 100),
     MODERATOR("Mod", ChatColor.DARK_PURPLE, 80),
