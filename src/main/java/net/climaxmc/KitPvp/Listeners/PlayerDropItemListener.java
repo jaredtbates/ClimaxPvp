5b96cda017655837e6cfe490eb1d2054a347c8e3
jonhan2000 <jtccornett2000@gmail.com>
2016-03-20 17:59:02 -0400

    Updating Kits (Finally)

--- src/main/java/net/climaxmc/KitPvp/Listeners/PlayerDropItemListener.java
+++ src/main/java/net/climaxmc/KitPvp/Listeners/PlayerDropItemListener.java
@@ -16,7 +16,7 @@ public class PlayerDropItemListener implements Listener {
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent event) {
         Material item = event.getItemDrop().getItemStack().getType();
-        if (item != Material.BOWL && item != Material.GLASS_BOTTLE) {
+        if (item != Material.BOWL && item != Material.GLASS_BOTTLE && item != Material.POTION) {
             event.setCancelled(true);
         }
     }
