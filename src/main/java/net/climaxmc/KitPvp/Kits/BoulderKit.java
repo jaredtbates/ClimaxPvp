09c96821b4ae4dbae2e894aceb22ebb24c4541d4
jonhan2000 <jtccornett2000@gmail.com>
2016-03-19 22:34:44 -0400

    Kit Editing

---
+++ src/main/java/net/climaxmc/KitPvp/Kits/BoulderKit.java
@@ -0,0 +1,108 @@
+package net.climaxmc.KitPvp.Kits;
+
+import me.xericker.disguiseabilities.DisguiseAbilities;
+import net.climaxmc.Administration.Commands.CheckCommand;
+import net.climaxmc.Administration.Commands.VanishCommand;
+import net.climaxmc.KitPvp.Kit;
+import net.climaxmc.KitPvp.KitManager;
+import net.climaxmc.KitPvp.KitPvp;
+import net.climaxmc.KitPvp.Utils.Ability;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.concurrent.TimeUnit;
+
+public class BoulderKit extends Kit {
+    private Ability bouldertoss = new Ability(1, 12, TimeUnit.SECONDS);
+
+    public BoulderKit() {
+        super("Boulder", new ItemStack(Material.DIRT), "Throw giant rocks at people to kill them!", ChatColor.GRAY);
+    }
+
+    protected void wear(Player player) {
+        ItemStack helm = new ItemStack(Material.IRON_HELMET);
+        player.getInventory().setHelmet(helm);
+        ItemStack chest = new ItemStack(Material.LEATHER_CHESTPLATE);
+        chest.addEnchantment(Enchantment.DURABILITY, 3);
+        chest.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+        LeatherArmorMeta chestmeta = (LeatherArmorMeta) chest.getItemMeta();
+        chestmeta.setColor(Color.NAVY);
+        chest.setItemMeta(chestmeta);
+        player.getInventory().setChestplate(chest);
+        ItemStack legs = new ItemStack(Material.LEATHER_LEGGINGS);
+        legs.addEnchantment(Enchantment.DURABILITY, 3);
+        legs.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+        LeatherArmorMeta legsmeta = (LeatherArmorMeta) legs.getItemMeta();
+        legsmeta.setColor(Color.NAVY);
+        legs.setItemMeta(legsmeta);
+        player.getInventory().setLeggings(legs);
+        player.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
+        ItemStack sword = new ItemStack(Material.IRON_SWORD);
+        player.getInventory().addItem(sword);
+        ItemStack ability = new ItemStack(Material.CLAY_BALL);
+        ItemMeta abilitymeta = ability.getItemMeta();
+        abilitymeta.setDisplayName(ChatColor.AQUA + "Boulder Ability");
+        ability.setItemMeta(abilitymeta);
+        player.getInventory().addItem(ability);
+        addSoup(player.getInventory(), 2, 35);
+    }
+
+    protected void wearNoSoup(Player player) {
+        for (PotionEffect effect : player.getActivePotionEffects()) {
+            player.removePotionEffect(effect.getType());
+        }
+        player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 0));
+        ItemStack helm = new ItemStack(Material.IRON_HELMET);
+        player.getInventory().setHelmet(helm);
+        ItemStack chest = new ItemStack(Material.LEATHER_CHESTPLATE);
+        chest.addEnchantment(Enchantment.DURABILITY, 3);
+        chest.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+        LeatherArmorMeta chestmeta = (LeatherArmorMeta) chest.getItemMeta();
+        chestmeta.setColor(Color.NAVY);
+        chest.setItemMeta(chestmeta);
+        player.getInventory().setChestplate(chest);
+        ItemStack legs = new ItemStack(Material.LEATHER_LEGGINGS);
+        legs.addEnchantment(Enchantment.DURABILITY, 3);
+        legs.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+        LeatherArmorMeta legsmeta = (LeatherArmorMeta) legs.getItemMeta();
+        legsmeta.setColor(Color.NAVY);
+        legs.setItemMeta(legsmeta);
+        player.getInventory().setLeggings(legs);
+        player.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
+        ItemStack sword = new ItemStack(Material.IRON_SWORD);
+        player.getInventory().addItem(sword);
+        ItemStack ability = new ItemStack(Material.CLAY_BALL);
+        ItemMeta abilitymeta = ability.getItemMeta();
+        abilitymeta.setDisplayName(ChatColor.AQUA + "Boulder Toss Ability");
+        ability.setItemMeta(abilitymeta);
+        player.getInventory().addItem(ability);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        final Player player = event.getPlayer();
+        if (KitManager.isPlayerInKit(player, this)) {
+            if (player.getInventory().getItemInHand().getType() == Material.CLAY_BALL) {
+                if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
+                    if (!bouldertoss.tryUse(player)) {
+                        return;
+                    }
+                    player.sendMessage(ChatColor.GOLD + "You used the " + ChatColor.AQUA + "Boulder Toss" + ChatColor.GOLD + " Ability!");
+                    DisguiseAbilities.activateAbility(player, DisguiseAbilities.ClassType.BULK);
+                }
+            }
+        }
+    }
+}
